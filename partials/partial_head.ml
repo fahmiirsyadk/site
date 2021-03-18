5bf4e13a71c427719cd0587f21f404d02bf4500c
Fahmi Irsyad Khairi <fahmiirsyad10@gmail.com>
2021-03-19 02:03:19 +0700

    Use google fonts

--- partials/partial_head.ml
+++ partials/partial_head.ml
@@ -12,6 +12,10 @@ let head ~title:(title: string) =
       P.href "https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@0;1&display=swap";
       P.rel "stylesheet";
     ] [];
+    H.link [
+      P.href "https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap";
+      P.rel "stylesheet";
+    ] [];
     H.title [] (H.text title);
     H.style [] (H.text (E.inject "assets/css/style.css"));
   ]
