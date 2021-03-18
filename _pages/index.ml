a5042232149cd56784f6ee5df8200f79ab04638e
Fahmi Irsyad Khairi <fahmiirsyad10@gmail.com>
2021-03-18 21:22:47 +0700

    Update style homepage

--- _pages/index.ml
+++ _pages/index.ml
@@ -8,11 +8,14 @@ let listPost posts =
     (fun (post: Parser.metadata) ->
        H.a [
          P.href post.url;
-         P.style "color: #444;"
        ] [
          H.li [] (H.text post.matter.title);
        ]
     )
+
+(* Variables *)
+let heading_title =
+    "Hi, i’m fahmi. On this site you will literally find collection of poetries, notes, thoughts, or anything comes up into my mind. " 
 
 let body posts =
   H.section [] [
@@ -23,8 +26,25 @@ let body posts =
     H.span [] (H.text "moon");
     H.span [] (H.text "shine");
    ];
-    H.h1 [] (H.text "Artikel");
-    H.ol [] (listPost posts);
+    H.div [ (P.class_ "section-home") ] [
+        H.div [ (P.class_ "section-home__left") ] [
+            H.div [ P.class_ "circle-blur circle-blur-red" ] [];
+            H.div [ (P.class_ "hero-heading") ] [
+                H.h1 [ P.class_ "hero-heading__title" ] [
+                    H.span [] ( H.text "The Space ");
+                    H.span [ P.style "font-style: italic;" ] (H.text "Garden.");
+                ];
+                H.div [ P.class_ "hero-heading__desc" ] [
+                    H.span [] (H.text heading_title);
+                    H.a [ P.href "/about"] (H.text "More about me -->");
+                ];
+            ];
+        ];
+        H.div [ (P.class_ "section-home__right") ] [            
+            H.h1 [] (H.text "Recent article");
+            H.ol [] (listPost posts);
+        ];
+    ];
   ]
 
 let html (posts: Parser.metadata array) =
--- layouts/layout_app.ml
+++ layouts/layout_app.ml
@@ -3,6 +3,11 @@ module P = Mana.Property
 
 let footer = Partial_footer.footer
 
+let navigation =
+  H.header [] [
+      H.nav [] (H.text "navigation")
+  ]
+
 let layout head section =
     H.html [] [
         head;
