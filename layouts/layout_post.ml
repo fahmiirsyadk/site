15a1a47c7a887bf6332702aba6d1281d7cb55a59
fahmiirsyadk <fahmiirsyad10@gmail.com>
2021-06-19 01:41:42 +0700

    Update head

--- layouts/layout_post.ml
+++ layouts/layout_post.ml
@@ -3,30 +3,12 @@ module P = Mana.Property
 module E = Mana.Extra
 
 let footer = Partial_footer.footer
-let css = E.inject "assets/css/style.css"
-
-let metaContent (ct: string) (nm:string) =
-  H.meta [
-    (P.content ct);
-    (P.name nm);
-  ] []
-
-let icon = H.link [
-  P.href "/assets/images/favicon.ico";
-  P.rel "shortcut icon";
-  P.type_ "image/x-icon";
-] []
+let head = Partial_head.head
 
 let template content (meta: Parser.metadata) =  
   [
   H.head [] [
-    H.meta [ P.charset "utf-8"] [];
-    H.title [] (H.text (meta.matter.title ^ "-- Fahmiirsyadk"));
-    metaContent "width=device-width,initial-scale=1" "viewport";
-    metaContent "#000" "theme-color";
-    metaContent "fahmiirsyadk -- a Web developer based on Banyuwangi, Indonesia." "description";
-    icon;
-    H.style [] (H.text css);
+    head ~title:(meta.matter.title ^ " -- Fahmiirsyadk");   
   ]; H.body [] [
     content;
     footer;
