15937052e8840fcadad603fbf4f7905488f40554
Fahmi Irsyad Khairi <fahmiirsyad10@gmail.com>
2021-10-10 19:16:16 +0000

    Add postcss minifier & purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+    process.env.NODE_ENV === 'production'
+      ? cssnano({ preset: 'default' })
+      : null,
+    purgecss({
+      content: ['./dist/**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './dist/**/*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
