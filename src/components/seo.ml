6880347a2ac91f5261496191edaca1e4b830a235
Fahmi Irsyad Khairi <fahmiirsyad10@gmail.com>
2021-12-05 14:43:49 +0000

    Add blog list url on homepage & other stuff

--- src/components/seo.ml
+++ src/components/seo.ml
@@ -2,24 +2,45 @@ module H = Dust.Html.Elements
 module A = Dust.Html.Attributes
 module E = Dust.Html.Extra
 
-let version = "0.1.9"
+let version = "0.2.0"
 let title = "fahmiirsyadk"
 let description = "FAHMIIRSYADK is a personal/blog website authored by fahmi irsyad khairi"
 let as_type = A.custom_attr("as")
 let cross_origin = A.custom_attr("crossOrigin")
 
-let head ~children () = 
-  H.head [] [
-    H.title [] (H.text title)
-    ; H.meta [ A.charset2 `UTF8 ] []
-    ; H.meta [ A.name "generator"; A.content {j|Dust $version|j} ] []
+let combine_elem elem list =
+  list |. Belt.List.reduce "" (fun a b -> let (k, v) = b in a ^ elem k v)
+(** 
+  * just showing loop
+  * same result as
+  * ; H.meta [ A.name "generator"; A.content {j|Dust $version|j} ] []
     ; H.meta [ A.name "viewport "; A.content "width=device-width, initial-scale: 1, shrink-to-fit=no"] []
     ; H.meta [ A.name "title"; A.content title] []
     ; H.meta [ A.name "description"; A.content description] []
+*)
+
+let meta_info = [
+    ("generator", {j|Dust $version|j})
+  ; ("author", "fahmi irsyad khairi")
+  ; ("description", description)
+  ; ("title", title)
+] |> combine_elem (fun a b -> H.meta [ A.name a; A.content b] [])
+
+let font_links = [
+    ("/assets/fonts/Inter.ttf", "font/ttf")
+  ; ("/assets/fonts/SwearBanner-Bold.otf", "font/otf")
+  ; ("/assets/fonts/SwearBanner-Italic.otf", "font/otf")
+  (* ; ("/assets/fonts/SwearBanner-MediumItalic.otf", "font/otf") *)
+] |> combine_elem (fun a b -> H.link [ A.rel_link `Preload; as_type "font"; A.href a; cross_origin "anonymous"; A.type_ b] [])
+
+let head ~children () = 
+  H.head [] [
+    H.title [] (H.text title)
+    ; meta_info
+    ; H.meta [ A.charset2 `UTF8 ] []
     ; E.meta_twitter ~title:title ~description:description ~card:"summary" ()
     ; H.link [ A.rel_link `Stylesheet; A.href "/assets/css/styles.css" ] []
-    ; H.link [ A.rel_link `Preload; as_type "font"; A.href "/assets/fonts/Inter.ttf"; cross_origin "anonymous"; A.type_ "font/ttf"] []
-    ; H.link [ A.rel_link `Preload; as_type "font"; A.href "/assets/fonts/SwearBanner-Bold.otf"; cross_origin "anonymous"; A.type_ "font/otf"] []
+    ; font_links
     ; H.link [ A.rel_link `Icon; A.type_ "image/x-icon"; A.href "/assets/images/logo.ico"] []
     ; children
   ]
--- src/pages/index.ml
+++ src/pages/index.ml
@@ -2,8 +2,45 @@ module H = Dust.Html.Elements
 module A = Dust.Html.Attributes
 module E = Dust.Html.Extra
 
-let desc = "Web developer / full-time frontend developer based in Indonesia, passionate to experiment with things, build solid & performant creative software."
-let main () =
+let desc = "Web developer / full-time frontend developer based in Indonesia, passionate about experiment with things, build solid & performant creative software."
+
+(** ARTICLES BLOG LISTS *)
+type matter = { title : string }
+
+type blog =
+  { name : string
+  ; layout : string
+  ; source : string
+  ; data : matter
+  ; excerpt : string
+  ; url : string
+  ; content : string
+  }
+
+type posts = { blog : blog array }
+
+let sections id data_list =
+  let emptyContents elem = 
+    match data_list |> List.length with
+    | 0 -> H.text "No post yet."
+    | _ -> elem
+  in
+  let list_links = data_list |. Belt.List.map (fun post ->
+    H.article [ A.class_ "mt-8"] [
+      H.h3 [ A.class_ "font-medium text-md"] [
+        H.a [ A.href post.url;] (H.text post.data.title)
+      ]
+    ; H.p [] (H.text "excerpt coming soon")
+    ]
+  ) in
+  H.div [] [
+    H.h2 [ A.class_ "font-swear italic mb-4 font-medium text-gray-800 text-2xl"] (H.text id)
+  ; H.div [] (emptyContents list_links)
+  ]
+
+let main posts =
+  let blog = posts.blog |> Array.to_list in
+
   H.html [] [
     Seo.head ~children:"" ()
   ; H.body [] [
@@ -13,9 +50,10 @@ let main () =
             A.class_ "position-absolute left-0 top-0 w-100 h-64"
           ; Aria.ariaHidden "true"
           ] []
-        ; H.h1 [ A.class_ "font-bold font-swear text-3xl mb-8"] (H.text "Fahmi Irsyad Khairi")
-        ; H.p [ A.class_ "px-64"] (H.text desc)
+        ; H.h1 [ A.class_ "font-bold font-swear text-3xl mb-8 text-gray-900"] (H.text "Fahmi Irsyad Khairi")
+        ; H.p [ A.class_ "lg:px-64 md:px-64 xl:px-64 sm:px-24 text-gray-600 font-medium"] (H.text desc)
         ]
+      ; H.section [ A.class_ "my-32 mx-64 flex justify-between"] (H.text ((sections "Articles" blog) ^ sections "Projects" []))
       ]
     ; Footer.elem
     ]
